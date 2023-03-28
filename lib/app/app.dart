d2bc0a73f952465ec854a58beec61aac85401223
Manuel Duarte  <carlosd99m@gmail.com>
2023-03-27 21:38:08 -0600

    adding basic widgets (#1)

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_app/config/router/app_router.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      theme: AppTheme(selectedColor: 0).getTheme(),
+    );
+  }
+}
